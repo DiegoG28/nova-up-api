58cd69cc85e65d1984a5cf26b8b57c36cc89d7ef
Diego <diegogutcat28@gmail.com>
2023-06-17 22:43:54 -0500

    Cats test module deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './modules/cats/cats.controller';
-import { CatsService } from './modules/cats/cats.service';
 import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './modules/students/students.module';
 
@@ -23,7 +21,7 @@ import { StudentsModule } from './modules/students/students.module';
     }),
     StudentsModule,
   ],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/modules/cats/dtos/UpdateCatDto.ts
+++
@@ -1,5 +0,0 @@
-export class UpdateCatDto {
-  name: string;
-  age?: string;
-  breed?: string;
-}
