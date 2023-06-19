ed3271cbace0dc2c719c43196c49128af3d78e13
Diego <diegogutcat28@gmail.com>
2023-06-19 09:48:19 -0500

    Student table renamed

--- src/modules/students/students.entity.ts
+++ src/modules/students/students.entity.ts
@@ -1,21 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'alumno' })
 export class Student {
   @ApiProperty()
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ name: 'id_alumno' })
   id: number;
 
   @ApiProperty()
-  @Column()
+  @Column({ name: 'matricula' })
   tuition: number;
 
   @ApiProperty()
-  @Column({ length: 150 })
+  @Column({ name: 'nombre_alumno', length: 150 })
   name: string;
 
   @ApiProperty()
-  @Column({ length: 150 })
+  @Column({ name: 'apellidos_alumno', length: 150 })
   lastName: string;
 }
