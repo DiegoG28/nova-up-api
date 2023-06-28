ea73ec8b19a94d02feb2ac2199bd38c985274a35
Diego <diegogutcat28@gmail.com>
2023-06-28 14:58:02 -0500

    Migrations configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './modules/students/students.module';
 import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './modules/posts/posts.module';
@@ -11,7 +10,6 @@ import { UsersModule } from './modules/users/users.module';
 
 @Module({
    imports: [
-      ConfigModule.forRoot(),
       StudentsModule,
       PostsModule,
       CareersModule,
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,26 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './db-config';
 
 @Module({
    imports: [
-      ConfigModule.forRoot(),
       TypeOrmModule.forRoot({
-         type: 'mysql',
-         host: process.env.DB_HOST,
-         port: parseInt(process.env.DB_PORT, 10),
-         username: process.env.DB_USERNAME,
-         password: process.env.DB_PASSWORD,
-         database: process.env.DB_NAME,
-         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-         synchronize: false,
+         ...dataSourceOptions,
          autoLoadEntities: true,
-         ssl: true,
-         extra: {
-            ssl: {
-               rejectUnauthorized: false,
-            },
-         },
       }),
    ],
 })
