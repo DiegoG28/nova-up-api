e5530d3013988f6a4615bf23e2411a9ee0d6a9a2
Diego <diegogutcat28@gmail.com>
2023-06-28 16:58:50 -0500

    Swagger packages removed

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './libs/swaggerConfig';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-   const app: NestExpressApplication = await NestFactory.create(AppModule);
+   const app = await NestFactory.create(AppModule);
 
    const document = SwaggerModule.createDocument(app, swaggerConfig);
    SwaggerModule.setup('api', app, document);
