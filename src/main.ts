3ebf5c3abaf50c31dcf15255d7fde8a27aaecf62
Diego <diegogutcat28@gmail.com>
2023-07-03 10:22:16 -0500

    Added route to store files in the API

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './libs/swaggerConfig';
+import * as express from 'express';
+import * as path from 'path';
 import 'dotenv/config';
 
 async function bootstrap() {
@@ -12,6 +14,8 @@ async function bootstrap() {
       customCssUrl:
          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.1.0/swagger-ui.min.css',
    });
+
+   app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
    await app.listen(parseInt(process.env.PORT) || 3000);
 }
