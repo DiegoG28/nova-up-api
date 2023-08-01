8d2f8edfdd8dbf6ef2c784e3bee8943db9d30f17
Diego <diegogutcat28@gmail.com>
2023-08-01 13:12:24 -0500

    Unused app controller and app service removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { UsersModule } from './modules/users/users.module';
@@ -15,7 +13,5 @@ import { AuthModule } from './modules/auth/auth.module';
       UsersModule,
       CatalogsModule,
    ],
-   controllers: [AppController],
-   providers: [AppService],
 })
 export class AppModule {}
