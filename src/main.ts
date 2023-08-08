b25a11deff37395c08a51eacc872478195c10118
Diego <diegogutcat28@gmail.com>
2023-08-08 10:58:38 -0500

    added console log

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
    const app = await NestFactory.create(AppModule, { cors: true });
 
+   console.log(swaggerConfig);
    const document = SwaggerModule.createDocument(app, swaggerConfig);
    SwaggerModule.setup('api-docs', app, document, {
       customCssUrl:
