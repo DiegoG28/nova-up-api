b49521f7e0b8f98a1f4753343f3e68efa80ee5ea
Diego <diegogutcat28@gmail.com>
2023-08-08 11:15:22 -0500

    Removed console log

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
    const app = await NestFactory.create(AppModule, { cors: true });
 
-   console.log(swaggerConfig);
    const document = SwaggerModule.createDocument(app, swaggerConfig);
    SwaggerModule.setup('api-docs', app, document, {
       customCssUrl:
