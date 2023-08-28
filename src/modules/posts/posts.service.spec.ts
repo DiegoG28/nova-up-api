5e6b1d7eb5ec770fa013345c96ec96838b429f2b
Diego <diegogutcat28@gmail.com>
2023-08-28 12:09:36 -0500

    Added demo testing in posts

---
+++ src/modules/posts/posts.service.spec.ts
@@ -0,0 +1,75 @@
+import { ForbiddenException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostsService } from './posts.service';
+import { PostsRepository } from './posts.repository';
+import { PostsMapperService } from './posts-mapper.service';
+import { DataSource } from 'typeorm';
+import { AssetsService } from '../assets/assets.service';
+import { CatalogsService } from '../catalogs/catalogs.service';
+
+describe('PostsService', () => {
+   let service: PostsService;
+   const mockPostsRepository = {
+      findAll: jest.fn(),
+   };
+   const mockPostsMapperService = {
+      mapToPostCardDto: jest.fn(),
+   };
+   const mockCatalogsService = {
+      findAllCategories: jest.fn(),
+   };
+   const mockAssetsService = {
+      findById: jest.fn(),
+   };
+   const mockQueryRunner = {
+      connect: jest.fn(),
+      startTransaction: jest.fn(),
+      commitTransaction: jest.fn(),
+      rollbackTransaction: jest.fn(),
+      release: jest.fn(),
+   };
+
+   const mockDataSource = {
+      createQueryRunner: jest.fn().mockReturnValue(mockQueryRunner),
+   };
+
+   beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+         providers: [
+            PostsService,
+            { provide: PostsRepository, useValue: mockPostsRepository },
+            { provide: PostsMapperService, useValue: mockPostsMapperService },
+            { provide: CatalogsService, useValue: mockCatalogsService },
+            { provide: AssetsService, useValue: mockAssetsService },
+            { provide: DataSource, useValue: mockDataSource },
+         ],
+      }).compile();
+
+      service = module.get<PostsService>(PostsService);
+   });
+
+   it('should throw ForbiddenException for unauthorized access to unapproved posts', async () => {
+      await expect(service.findAll('Editor', false)).rejects.toThrow(
+         ForbiddenException,
+      );
+   });
+
+   it('should allow authorized roles to access all posts', async () => {
+      const mockPosts = [{ id: 1 }, { id: 2 }];
+      const mockPostCardDto = [
+         { id: 1, cardInfo: 'info' },
+         { id: 2, cardInfo: 'info' },
+      ];
+
+      mockPostsRepository.findAll.mockReturnValue(Promise.resolve(mockPosts));
+      mockPostsMapperService.mapToPostCardDto.mockReturnValue(mockPostCardDto);
+
+      const result = await service.findAll('Admin', false);
+      console.log(mockPostsMapperService);
+      expect(result).toEqual(mockPostCardDto);
+      expect(mockPostsRepository.findAll).toHaveBeenCalledWith(false);
+      expect(mockPostsMapperService.mapToPostCardDto).toHaveBeenCalledWith(
+         mockPosts,
+      );
+   });
+});
